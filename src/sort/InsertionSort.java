f348a9d1fccf920c7e9ae34a8941baae5df52390
sungwooIsGood <tjddn3927@naver.com>
2022-02-21 21:31:00 +0900

    merge정렬

--- src/sort/InsertionSort.java
+++ src/sort/InsertionSort.java
@@ -25,15 +25,12 @@ public class InsertionSort {
 	      for(int i = 1; i < arr.length; i++) {
 	         
 	    	 int key = arr[i]; // key값 3
-	    	
-	    	 //int[] pre = new int[i];// key값 비교할 이전 인덱스 2
 	    	  
-	    	  
-	    	  for(int j = pre; j >= 0; j--) { // 2
+	    	  for(int j = pre; j >= 0; j--) { 
 	    		  
-	    		  if(arr[j] > key) { // arr[0](3) > 2                                 /  j=1 arr[1](3) > arr[2](1)               / j=0 arr[0](2) > key(arr[2] 1)                                              
-	    			  arr[j+1] = arr[j]; // arr[1] = arr[0]      => arr[0] = 3 , 3-3-1  /  arr[2](1) = arr[1](3) => 2-3-3          / arr[2](3) = arr[0](2) => 2-1-2 
-	    			  arr[j] = key; // arr[0] = key(arr[1] 2')  => arr[1] = 3 , 2-3-1  /  arr[1](3) = key(arr[2] 1) => 2-1-3      / arr[0](2) = key(arr[1] 1) => 1-1-2 
+	    		  if(arr[j] > key) {                                      
+	    			  arr[j+1] = arr[j]; 
+	    			  arr[j] = key; 
 	    		  }
 	    		
 	    	  }
---
+++ src/sort/MergeSort.java
@@ -0,0 +1,67 @@
+package sort;
+
+public class MergeSort {
+	
+	// 분할하는 메소드
+	public static void mergesort(int[] arr, int[]temp, int first, int last) { 
+		
+		
+		if(first < last) {
+			int mid = (first + last) / 2; 
+			// first, mid, last는 arr의 index를 가르킨다.
+			mergesort(arr,temp,first,mid); 
+			mergesort(arr,temp,mid+1,last);
+			merge(arr,temp,first,mid,last);
+		}
+  	}
+	
+	// 병합하는 메소드
+	public static void merge(int[] arr, int[] temp, int first, int mid, int last) {
+		for(int i = first; i <=last; i++) {
+			temp[i] = arr[i];
+//			System.out.print(first);
+		}
+		
+		int left = first;
+		int right = mid +1;
+		int index = first;
+		
+		while(left <= mid && right <= last) {
+			if(temp[left] <= temp[right]) {
+				arr[index] = temp[left];
+				left++;
+			} else {
+				arr[index] = temp[right];
+				right++;
+			}
+			index++;
+		}
+		for(int i = 0; i <=mid-left; i++) {
+			arr[index+i] = temp[left+i];
+		}
+	}
+	
+	public static void main(String[] args) {
+		
+		// 정렬안된 배열
+		int[] arr = {5,4,3,6,7,8};
+		// 정렬할 배열
+		int[] temp = new int[arr.length];
+
+		System.out.println("정렬 전 배열");
+		for(int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
+		System.out.println();
+		System.out.println();
+		
+		mergesort(arr,temp,0,arr.length-1);
+		
+		System.out.println("정렬 후 배열");
+		for(int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
+		
+	}
+
+}
