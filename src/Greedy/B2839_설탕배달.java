f46d19a241cfe2d56276fbab13516baba932a3eb
sungwooIsGood <tjddn3927@naver.com>
2022-03-10 23:16:39 +0900

    greedy백준2839번

---
+++ src/Greedy/B2839_설탕배달.java
@@ -0,0 +1,35 @@
+package Greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B2839_설탕배달 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// 모든 경우의 수를 탐색 시 시간을 줄이기 위해 사용
+		// a에서 b까지 탐색 시 기준을 가지고 가장 적은 경우의 수부터 탐색하는 것
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int input = Integer.parseInt(br.readLine());
+		
+		int temp = 0;
+		
+		while(true) {
+			if(input % 5 == 0) {
+				temp += input / 5;
+				System.out.println(temp);
+				break;
+			} else {
+				input -= 3;
+				temp++;
+			}
+			
+			if(input < 0) {
+				System.out.println(-1);
+				break;
+			}
+		}
+	}
+
+}
